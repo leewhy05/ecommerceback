fda8e736b7d3f0c385905441b46f338f20d6470c
leewhy05 <leewhy1105@gmail.com>
2023-12-20 04:02:29 -0700

    fixes

--- controller/userController.js
+++ controller/userController.js
@@ -121,7 +121,7 @@ const forgotPassword = async(req,res)=>{
         }
         const resetToken = user.getResetPasswordToken()
         await user.save()
-        const resetUrl = `http://localhost:5173/password/${resetToken}`
+        const resetUrl = `https://ecommercefront-vert.vercel.app/password/${resetToken}`
         const message = `<h1> You have requested for a password reset</h1>
         <p> Please go to this link to reset your password</p> <a href=${resetUrl} clicktracking = off> ${resetUrl} </a> `
         try {
